c4c1bc28cf895df35d53b2d854c05aee44ebe2ed
Vadim Pisarevsky <vadim.pisarevsky@me.com>
2021-01-19 18:42:58 +0800

    1. added live mode 2. added proper diagnostic when the model is not found

--- modules/wechat_qrcode/samples/qrcode_example.cpp
+++ modules/wechat_qrcode/samples/qrcode_example.cpp
@@ -19,18 +19,58 @@ int main(int argc, char* argv[]) {
     cout << "A demo program of WeChat QRCode Detector: " << endl;
 
     Mat img;
+    int camIdx = -1;
     if (argc > 1) {
-        img = imread(argv[1]);
+        bool live = strcmp(argv[1], "-camera") == 0;
+        if (live) {
+            camIdx = argc > 2 ? atoi(argv[2]) : 0;
+        } else {
+            img = imread(argv[1]);
+        }
     } else {
         cout << "    Usage: " << argv[0] << " <input_image>" << endl;
         return 0;
     }
     // The model is downloaded to ${CMAKE_BINARY_DIR}/downloads/wechat_qrcode if cmake runs without warnings,
     // otherwise you can download them from https://github.com/WeChatCV/opencv_3rdparty/tree/wechat_qrcode.
-    auto detector = wechat_qrcode::WeChatQRCode("detect.prototxt", "detect.caffemodel",
-                                                "sr.prototxt", "sr.caffemodel");
+    Ptr<wechat_qrcode::WeChatQRCode> detector;
+
+    try {
+        detector = makePtr<wechat_qrcode::WeChatQRCode>("detect.prototxt", "detect.caffemodel",
+                                                        "sr.prototxt", "sr.caffemodel");
+    } catch (const std::exception& e) {
+        cout <<
+            "\n---------------------------------------------------------------\n"
+            "Failed to initialize WeChatQRCode.\n"
+            "Please, download 'detector.*' and 'sr.*' from\n"
+            "https://github.com/WeChatCV/opencv_3rdparty/tree/wechat_qrcode\n"
+            "and put them into the current directory.\n"
+            "---------------------------------------------------------------\n";
+        return 0;
+    }
+    string prevstr = "";
     vector<Mat> points;
-    auto res = detector.detectAndDecode(img, points);
-    for (const auto& t : res) cout << t << endl;
+
+    if (camIdx < 0) {
+        auto res = detector->detectAndDecode(img, points);
+        for (const auto& t : res) cout << t << endl;
+    } else {
+        VideoCapture cap(camIdx);
+        for(;;) {
+            cap >> img;
+            if (img.empty())
+                break;
+            auto res = detector->detectAndDecode(img, points);
+            for (const auto& t : res) {
+                if (t != prevstr)
+                    cout << t << endl;
+            }
+            if (!res.empty())
+                prevstr = res.back();
+            imshow("image", img);
+            if (waitKey(30) >= 0)
+                break;
+        }
+    }
     return 0;
 }
